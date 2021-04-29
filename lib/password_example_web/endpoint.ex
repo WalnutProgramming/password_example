5df8975114c8255f76e13858fbc58758314ce384
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-04-28 23:40:51 -0400

    Prepare for Heroku
    https://hexdocs.pm/phoenix/heroku.html

--- lib/password_example_web/endpoint.ex
+++ lib/password_example_web/endpoint.ex
@@ -14,7 +14,7 @@ defmodule PasswordExampleWeb.Endpoint do
   ]
 
   socket "/socket", PasswordExampleWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
