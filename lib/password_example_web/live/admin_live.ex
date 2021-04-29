7221ef1868e2d61c5246832461722d0a10954371
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2021-04-29 18:16:30 -0400

    Improve admin UI

--- lib/password_example_web/live/admin_live.ex
+++ lib/password_example_web/live/admin_live.ex
@@ -10,10 +10,18 @@ defmodule PasswordExampleWeb.AdminLive do
   end
 
   @impl true
-  def handle_event("change_hash_type", %{"value" => hash_type}, socket) do
-    hash_type = String.to_atom(hash_type)
+  def handle_event("change_hash_type", %{"change_hash_type" => %{"hash_type" => hash_type_str}}, socket) do
+    hash_type = String.to_atom(hash_type_str)
     HashingAlgorithmChoice.set(hash_type)
+    socket = socket
+      |> assign(hash_type: hash_type)
+      |> put_flash(:info, "Changed hash type to " <> hash_type_str)
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("delete_all_users", _, socket) do
     User.delete_all()
-    {:noreply, assign(socket, hash_type: hash_type)}
+    {:noreply, socket}
   end
 end
